b8e7d02d7a71851b5cd228b8790bb74930da24b3
nicoisz <nicolasignacio.sz@gmail.com>
2024-05-16 22:12:12 -0600

    chapter 3-2

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana, Montserrat } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const monserrat = Montserrat({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400',
+});
