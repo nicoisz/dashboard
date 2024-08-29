bc5a8c5fc42cc42cab7f002ab2195531f9dcd1df
nicoisz <nicolasignacio.sz@gmail.com>
2024-08-29 16:10:15 -0600

    starting again

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++
@@ -1,5 +0,0 @@
-import { lusitana } from '../../ui/fonts';
-
-export default function costumerPage() {
-  return <div className={`${lusitana.className}`}>Costumer Page</div>;
-}
--- nextjs-dashboard/app/ui/fonts.ts
+++
@@ -1,9 +0,0 @@
-import { Inter, Lusitana, Montserrat } from 'next/font/google';
-
-export const inter = Inter({ subsets: ['latin'] });
-export const monserrat = Montserrat({ subsets: ['latin'] });
-
-export const lusitana = Lusitana({
-  subsets: ['latin'],
-  weight: '400',
-});
